db8341f44332dd121e43bb926149c09af263abdc
Hadi Ali <hadiali@hadis-mac.local>
2018-10-06 04:01:40 -0700

    pods install

---
+++ build/Pods.build/Debug-iphoneos/Pods-NearbyPlaces.build/DerivedSources/Pods_NearbyPlaces_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_NearbyPlacesVersionString[];
+ extern const double Pods_NearbyPlacesVersionNumber;
+
+ const unsigned char Pods_NearbyPlacesVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_NearbyPlaces  PROJECT:Pods-1" "\n";
+ const double Pods_NearbyPlacesVersionNumber __attribute__ ((used)) = (double)1.;
